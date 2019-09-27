2c493ec258b434e913ca70cf20f5d2c72fdb3ee9
laserbeam2045 <laserbeam2045@gmail.com>
2019-09-28 08:47:59 +0900

    feat: output.c内の処理で、インスタンスから直接属性を取得していたのを、関数呼び出しをするように変更

--- api/C/src/io/output.c
+++ api/C/src/io/output.c
@@ -21,33 +21,37 @@ void writeNodeDataStr(char buf[], size_t bufSize, SearchNode *node, double elaps
   size_t n = strlen(buf);     // 書き込みを始める位置として使う
   int i;
 
+  Board *board = SearchNode_getBoard(node);
   n += _snprintf(buf + n, bufSize, "{\n\"board\"\t\t\t: [");
   for (i = 0; i < Board_length; i++) {
-    n += _snprintf(buf + n, bufSize, "%d", node->board.state[i]);
+    n += _snprintf(buf + n, bufSize, "%d", Board_getColor(board, i));
     if (i != (Board_length - 1)) n += _snprintf(buf + n, bufSize, ", ");
   }
   n += _snprintf(buf + n, bufSize, "],\n");
 
-  int processLen = sizeof(node->process) / sizeof(node->process[0]);
+  const char *process = SearchNode_getProcess(node);
   n += _snprintf(buf + n, bufSize, "\"process\"\t\t: [");
-  for (i = 0; i < processLen; i++) {
-    n += _snprintf(buf + n, bufSize, "%d", node->process[i]);
-    if (i != (processLen - 1)) n += _snprintf(buf + n, bufSize, ", ");
+  for (i = 0; i < PROCESS_LEN_MAX; i++) {
+    n += _snprintf(buf + n, bufSize, "%d", process[i]);
+    if (i != (PROCESS_LEN_MAX - 1)) n += _snprintf(buf + n, bufSize, ", ");
     if (i == 29) n += _snprintf(buf + n, bufSize, "\n\t\t\t   ");
   }
   n += _snprintf(buf + n, bufSize, "],\n");
 
   n += _snprintf(buf + n, bufSize,
-        "\"movedCount\"\t\t: %d,\n"
-        "\"movedCountDiagonally\"\t: %d,\n"
-        "\"hashValue\"\t\t: \"%08x%08x\",\n",
-        node->movedCount, node->movedCountDiagonally,
-        (int)(node->hashValue >> 32), (int)node->hashValue);
+    "\"movedCount\"\t\t: %d,\n"
+    "\"movedCountDiagonally\"\t: %d,\n"
+    "\"hashValue\"\t\t: \"%08x%08x\",\n",
+    SearchNode_getMovedCount(node),
+    SearchNode_getMovedCountDiagonally(node),
+    (int)(*SearchNode_getHashValue(node) >> 32),
+    (int)(*SearchNode_getHashValue(node))
+  );
 
   n += _snprintf(buf + n, bufSize,
         "\"elapsedTime\"\t\t: %3.2lf,\n", elapsedTime);
   
-  writeComboDataStr(buf, bufSize, n, &node->comboData);
+  writeComboDataStr(buf, bufSize, n, SearchNode_getComboData(node));
   _snprintf(buf + strlen(buf), bufSize, "}\n");
 }
 
@@ -58,68 +62,89 @@ static void writeComboDataStr(char buf[], size_t bufSize, size_t n, ComboData *d
   int i, j;
 
   n += _snprintf(buf + n, bufSize,
-        "\"comboData\": {\n"
-        "\t\"step\"\t\t: %d,\n",
-        data->step);
+    "\"comboData\": {\n"
+    "\t\"step\"\t\t: %d,\n",
+    ComboData_getStep(data)
+  );
 
   n += _snprintf(buf + n, bufSize, "\t\"twoWay\"\t: [");
-  for (i = 0; i < 7; i++) {
-    n += _snprintf(buf + n, bufSize, "%d", data->twoWay[i]);
-    if (i != 6) n += _snprintf(buf + n, bufSize, ", ");
+  for (i = 0; i < 8; i++) {
+    n += _snprintf(buf + n, bufSize, "%d",
+      ComboData_getClearStyle(data, (CS_TYPE)TWO_WAY, i)
+    );
+    if (i != 7) n += _snprintf(buf + n, bufSize, ", ");
   }
   n += _snprintf(buf + n, bufSize, "],\n");
 
   n += _snprintf(buf + n, bufSize, "\t\"breakThrough\"\t: [");
-  for (i = 0; i < 7; i++) {
-    n += _snprintf(buf + n, bufSize, "%d", data->breakThrough[i]);
-    if (i != 6) n += _snprintf(buf + n, bufSize, ", ");
+  for (i = 0; i < 8; i++) {
+    n += _snprintf(buf + n, bufSize, "%d",
+      ComboData_getClearStyle(data, (CS_TYPE)BREAK_THROUGH, i)
+    );
+    if (i != 7) n += _snprintf(buf + n, bufSize, ", ");
   }
   n += _snprintf(buf + n, bufSize, "],\n");
 
   n += _snprintf(buf + n, bufSize, "\t\"line\"\t\t: [");
   for (i = 0; i < 8; i++) {
-    n += _snprintf(buf + n, bufSize, "%d", data->line[i]);
+    n += _snprintf(buf + n, bufSize, "%d",
+      ComboData_getClearStyle(data, (CS_TYPE)LINE, i)
+    );
     if (i != 7) n += _snprintf(buf + n, bufSize, ", ");
   }
   n += _snprintf(buf + n, bufSize, "],\n");
 
   n += _snprintf(buf + n, bufSize, "\t\"cross\"\t\t: [");
-  for (i = 0; i < 7; i++) {
-    n += _snprintf(buf + n, bufSize, "%d", data->cross[i]);
-    if (i != 6) n += _snprintf(buf + n, bufSize, ", ");
+  for (i = 0; i < 8; i++) {
+    n += _snprintf(buf + n, bufSize, "%d",
+      ComboData_getClearStyle(data, (CS_TYPE)CROSS, i)
+    );
+    if (i != 7) n += _snprintf(buf + n, bufSize, ", ");
   }
   n += _snprintf(buf + n, bufSize, "],\n");
 
   n += _snprintf(buf + n, bufSize, "\t\"L\"\t\t: [");
-  for (i = 0; i < 7; i++) {
-    n += _snprintf(buf + n, bufSize, "%d", data->L[i]);
-    if (i != 6) n += _snprintf(buf + n, bufSize, ", ");
+  for (i = 0; i < 8; i++) {
+    n += _snprintf(buf + n, bufSize, "%d",
+      ComboData_getClearStyle(data, (CS_TYPE)L, i)
+    );
+    if (i != 7) n += _snprintf(buf + n, bufSize, ", ");
   }
   n += _snprintf(buf + n, bufSize, "],\n");
 
   n += _snprintf(buf + n, bufSize, "\t\"combo\"\t: [\n");
   for (i = 0; i < DROP_TYPE_MAX + 1; i++) {
     n += _snprintf(buf + n, bufSize, "\t\t\t\t[");
-    for (j = 0; j < 10; j++) {
-      n += _snprintf(buf + n, bufSize, "%d", data->combo[i][j]);
-      if (j != 9) n += _snprintf(buf + n, bufSize, ", ");
+    for (j = 0; j < COUNT_COMBO_MAX; j++) {
+      n += _snprintf(buf + n, bufSize, "%d",
+        ComboData_getComboOf(data, i, j)
+      );
+      if (j != (COUNT_COMBO_MAX - 1)) {
+        n += _snprintf(buf + n, bufSize, ", ");
+      }
     }
-    if (i != DROP_TYPE_MAX) n += _snprintf(buf + n, bufSize, "],\n");
-    else                    n += _snprintf(buf + n, bufSize, "]\n");
+    if (i != DROP_TYPE_MAX)
+      n += _snprintf(buf + n, bufSize, "],\n");
+    else 
+      n += _snprintf(buf + n, bufSize, "]\n");
   }
   n += _snprintf(buf + n, bufSize, "\t\t\t  ],\n");
 
   n += _snprintf(buf + n, bufSize, "\t\"leftovers\"\t: [");
   for (i = 0; i < DROP_TYPE_MAX + 1; i++) {
-    n += _snprintf(buf + n, bufSize, "%d", data->leftovers[i]);
+    n += _snprintf(buf + n, bufSize, "%d", ComboData_getLeftovers(data, i));
     if (i != DROP_TYPE_MAX) n += _snprintf(buf + n, bufSize, ", ");
   }
   n += _snprintf(buf + n, bufSize, "],\n");
 
   n += _snprintf(buf + n, bufSize,
-        "\t\"maxConnection\"\t: %d,\n"
-        "\t\"magnification\"\t: %f,\n"
-        "\t\"evaluation\"\t: %f,\n"
-        "\t\"fulfillConditions\"\t: %d\n}\n",
-        data->maxConnection, data->magnification, data->evaluation, data->fulfillConditions);
+    "\t\"maxConnection\"\t: %d,\n"
+    "\t\"magnification\"\t: %f,\n"
+    "\t\"evaluation\"\t: %f,\n"
+    "\t\"fulfillConditions\"\t: %d\n}\n",
+    ComboData_getMaxConnection(data),
+    ComboData_getMagnification(data),
+    ComboData_getEvaluation(data),
+    ComboData_getFulfillConditions(data)
+  );
 }
